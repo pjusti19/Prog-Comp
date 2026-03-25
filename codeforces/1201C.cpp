27a494a3a7addd834e6213ec56f2756668495dae
pjusti19 <pedrinhojusti@gmail.com>
2026-03-25 09:53:20 -0300

    1201C

--- codeforces/1201C.cpp
+++ codeforces/1201C.cpp
@@ -1,50 +1,49 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-  int array_size, operations_num;
-  cin >> array_size >> operations_num;
-  vector<long long> numbers(array_size);
-  for (long long &number : numbers)
-    cin >> number;
+    int array_size, operations_num;
+    cin >> array_size >> operations_num;
+    vector<long long> numbers(array_size);
+    for (long long &number : numbers)
+        cin >> number;
 
-  int mid = array_size / 2;
-  sort(numbers.begin(), numbers.end());
+    int mid = array_size / 2;
+    sort(numbers.begin(), numbers.end());
 
-  while (operations_num > 0) {
-    auto it = upper_bound(numbers.begin() + mid, numbers.end(), numbers[mid]);
-    int index = it - numbers.begin();
-    if (index == numbers.size())
-      index--;
-    long long dif = numbers[index] - numbers[mid];
-    int i = index - 1;
-    if (dif == 0) {
-      dif = 1;
-      i = index;
+    if (array_size == 1)
+    {
+        cout << numbers[0] + operations_num << "\n";
+        return 0;
     }
-    for (; i >= mid; i--) {
-      if (operations_num == 0)
-        break;
-      numbers[i] += dif;
-      operations_num -= dif;
-    }
-  }
 
-  cout << numbers[mid] << "\n";
-  return 0;
+    while (operations_num > 0)
+    {
+        int index = upper_bound(numbers.begin() + mid, numbers.end(), numbers[mid]) - numbers.begin();
+        long long width = index - mid;
+
+        if(index == array_size){
+            numbers[mid] += operations_num / width;
+            break;
+        }
+
+        long long need = width * (numbers[index] - numbers[mid]);
+
+        if(need <= operations_num){
+            numbers[mid] = numbers[index];
+            operations_num -= need;
+        }
+        else{
+            numbers[mid] += operations_num / width;
+            operations_num = 0;
+        }
+        
+    }
+
+    cout << numbers[mid] << "\n";
+    return 0;
 }
-
-/* 5 5
-1 2 1 1 1
-3
-
-3 2
-1 3 5
-5
-
-7 7
-4 1 2 4 3 4 4
-5 */
