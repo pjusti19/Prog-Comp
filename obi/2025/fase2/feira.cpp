906fdb3db502e4ae8473c597b4c766ed4217084b
pjusti19 <pedrinhojusti@gmail.com>
2026-04-19 13:05:54 -0300

    heap obi

--- obi/2025/fase2/feira.cpp
+++ obi/2025/fase2/feira.cpp
@@ -1,34 +1,54 @@
-#include <bits/stdc++.h>
+#include <functional>
+#include <iostream>
+#include <set>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <queue>
+
+#define ll long long
 using namespace std;
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-    long long estoque, num_tipos;
-    cin >> estoque >> num_tipos;
-    vector<pair<long long, int>> tipos(estoque, {0, 0});
-    vector<long long> precos(estoque);
-    for (int i = 0; i < estoque; i++)
-    {
-        long long tipo;
-        cin >> tipo;
-        tipos[i].first = tipo;
-        tipos[i].second++;
+  ll n, t;
+  cin >> n >> t;
+  vector<ll> tipos(n);
+  vector<ll> precos(n);
+  for(ll &tipo : tipos) cin >> tipo;
+  for(ll &preco : precos) cin >> preco;
+  unordered_map<ll, priority_queue<ll, vector<ll>, greater<ll>>> por_tipo;
+  set<pair<ll, ll>> global;
+  for(int i = 0; i < n; i++) por_tipo[tipos[i]].push(precos[i]);
+  for(auto &it : por_tipo) global.insert({it.second.top(), it.first});
+
+  ll sum = 0;
+  ll c;
+  cin >> c;
+
+  for(int i = 0; i < c; i++){
+    ll u;
+    cin >> u;
+    if(u == 0 && !global.empty()){
+      auto item = *global.begin();
+      por_tipo[item.second].pop();
+      sum += item.first;
+      global.erase(item);
+      if(!por_tipo[item.second].empty())
+        global.insert({por_tipo[item.second].top(), item.second});
     }
-    for (long long &p : precos) cin >> p;
-    long long num_clientes;
-    cin >> num_clientes;
-    vector<long long> clientes(num_clientes);
-    for (long long &c : clientes) cin >> c;
+    else if(!por_tipo[u].empty()){
+      ll p = por_tipo[u].top();
+      por_tipo[u].pop();
+      sum += p;
+      global.erase({p, u});
+      if(!por_tipo[u].empty()) global.insert({por_tipo[u].top(), u});
+    }
+  }
 
-
-
-
-
-    return 0;
+  cout << sum << "\n";
+  return 0;
 }
-
-//priority_queue<int, vector<int>, greater<int>>
-//set<pair<int,int>> // (preço, tipo)
